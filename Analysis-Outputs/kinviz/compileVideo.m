f6be00c94e5057765afe71b9c3e86a067394371b
jmgoodman <j.mike.goodman@gmail.com>
2022-03-18 09:13:59 +0100

    Create compileVideo.m

---
+++ Analysis-Outputs/kinviz/compileVideo.m
@@ -0,0 +1,4 @@
+% trawls directories
+% takes the last frame of each .mot
+% puts them all together into a single .mot
+% now all you gotta do is make the movie in Opensim and extract the individual frames of that movie

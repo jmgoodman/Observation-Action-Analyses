6d44f6151cc1727b83af373b593093c1432ee5b9
jmgoodman <j.mike.goodman@gmail.com>
2022-04-13 10:34:04 +0200

    Update thetacomp.m

--- Utils/thetacomp.m
+++ Utils/thetacomp.m
@@ -1,5 +1,14 @@
 function theta = thetacomp(X,k,parallelflag)
 % subfunction of PAIRStest
+%
+% I seem to recall that Euclidean distances was a bad idea, but I don't
+% remember why.
+%
+% In any case, the PAIRS paper has a similar use case (i.e., not JUST
+% preferred directions in 2D space) and still uses cosine distance to test
+% clustering, so in order to just say "we used the PAIRS test", I should...
+% just use the dang PAIRS test.
+
 pdX    = pdist(X,'cosine'); % gives 1-cos
 cosX   = 1-squareform(pdX); % 1 - (1 - cos) = cos
 thetaX = acos(cosX);

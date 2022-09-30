f9635b210f37a7f79f3336754031d970fe9c4c33
jmgoodman <j.mike.goodman@gmail.com>
2022-09-30 17:14:08 +0200

    Update decodernet.py

--- MirrorData/network-decoder/decodernet.py
+++ MirrorData/network-decoder/decodernet.py
@@ -4,14 +4,21 @@ import torch.nn.functional as F
 
 from typing import Optional
 
-# decide device
-if torch.cuda.is_available():
-    DEVICE = "cuda"
-else:
-    DEVICE = "cpu"
+# helper method: set device depending on hardware
+def set_device() -> str:
+    """set_device spits out a device name for use with PyTorch tensors
+
+    Returns:
+        str: "cuda" or "cpu" depending on whether cuda hardware is available or not. Default is to return "cuda" if hardware is available, otherwise, return "cpu"
+    """
+    if torch.cuda.is_available():
+        DEVICE = "cuda"
+    else:
+        DEVICE = "cpu"
+    return DEVICE
     
 # helper method: mask-making
-def make_mask(src_size:int,tgt_size:Optional[int]=None,diag_ind:int=0):
+def make_mask(src_size:int,tgt_size:Optional[int]=None,diag_ind:int=0) -> torch.DoubleTensor:
     """make_mask defines a masking paradigm to make sure transformer decoders aren't basing their predictions of the future... on information from the future 
 
     Args:
@@ -22,7 +29,7 @@ def make_mask(src_size:int,tgt_size:Optional[int]=None,diag_ind:int=0):
     if tgt_size==None:
         tgt_size = src_size
         
-    mask = torch.zeros( (tgt_size,src_size), dtype=torch.float )
+    mask = torch.zeros( (tgt_size,src_size), dtype=torch.double ) # hopefully this works with float('-inf')
     for i in range(tgt_size):
         for j in range(src_size):
             if (j+diag_ind) >= i:
@@ -31,27 +38,38 @@ def make_mask(src_size:int,tgt_size:Optional[int]=None,diag_ind:int=0):
     return mask
     
 class DecoderNet(nn.Module):
-    def __init__(self,dimensionality_neural:int,dimensionality_kinematic:int):
+    def __init__(self,dimensionality_neural:int,dimensionality_kinematic:int,d_model:int=30,n_head:int=6,
+                 num_encoder_layers:int=3,num_decoder_layers:int=3,dropout:float=0.1,device:Optional[str]=None):
         super(DecoderNet, self).__init__()
         self.dimensionality_neural = dimensionality_neural
         self.dimensionality_kinematic = dimensionality_kinematic
-        self.
+        self.neural_compressor      = nn.Linear(in_features=dimensionality_neural,out_features=d_model)
+        self.kinematic_compressor   = nn.Linear(in_features=dimensionality_kinematic,out_features=d_model)
+        self.kinematic_decompressor = nn.Linear(in_features=d_model,out_features=dimensionality_kinematic)
         
-    
-    
-
-mdl = nn.Transformer(d_model = 30,
-                     nhead=8,
-                     num_encoder_layers=3,
-                     num_decoder_layers=3,
-                     dim_feedforward=100,
-                     dropout=0.1,
-                     activation="relu",
-                     custom_encoder=None,
-                     custom_decoder=None,
-                     layer_norm_eps=1e-06,
-                     batch_first=True,
-                     norm_first=False,
-                     device=DEVICE,
-                     dtype=None
-                     )
+        if device==None:
+            device = set_device()
+        
+        self.big_fat_model          = nn.Transformer(d_model = d_model,
+                                                     n_head = n_head,
+                                                     num_encoder_layers = num_encoder_layers,
+                                                     num_decoder_layers = num_decoder_layers,
+                                                     dropout=dropout,
+                                                     activation="relu",
+                                                     custom_encoder=None,
+                                                     custom_decoder=None,
+                                                     layer_norm_eps=1e-06,
+                                                     batch_first=True,
+                                                     norm_first=False,
+                                                     device=device,
+                                                     dtype=None
+                                                     ) # norm_first does not control whether layer normalization happens, but rather whether it happens before (True) or after (False) attention & feedforward operations
+        
+    def forward(self,neur:torch.DoubleTensor,kin:torch.DoubleTensor):
+        neur = self.neural_compressor(neur)
+        kin  = self.kinematic_compressor(kin)
+        
+        seqlen = neur.shape[1]
+        
+        neurmask = make_mask(seqlen,diag_ind=1)
+        

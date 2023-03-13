4e4efcc7fabdc8234e6fa8e87ac8c2874f6a9619
jmgoodman <j.mike.goodman@gmail.com>
2023-03-13 16:59:12 +0100

    Update subspacealignment.py

--- pythonfigures/subspacealignment.py
+++ pythonfigures/subspacealignment.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import List
+from typing import List, Optional
 
 import mat73
 import pandas as pd
@@ -10,23 +10,35 @@ import os
 # what is the subspace alignment index?
 # essentially, it's the ratio of variance explained by some arbitrary n-dimensional manfiold w.r.t. that explained by the n-dimensional pca decomposition of the data
 
-# todo: make Data's internals based on pandas to enable sql-like selection, aggregation, & grouping
+# TODO: refactor refactor refactor! have "data" be a parent data object, then "view" be a subclass that adds dimensional grouping, epoch selection, array selection, and context selection
+# Hmmm, is "view" a subclass of data, or rather, a wrapper around data? I think it's the latter, actually
 
 class Data:
-    def __init__(self):
-        self.filename = 'muster'
-        self._view    = [[]]
-        self.data     = np.array([])
-        self.subspace = np.array([])
-    def load(self,filename:str):
-        pass
-    def setview(self,dimension_groupings:List[List[int]]):
-        pass
-    def applyview(self) -> np.array:
-        pass
-    def infer_pca_subspace(self):
-        pass
-    def compute_pca_variance_captured(self) -> List[float]:
-        pass
-    def compute_subspace_alignment(self,data:Data) -> float:
-        pass
+    def __init__(self,
+                 filename:str = os.path.join('MirrorData','Zara70_datastruct.mat')
+                 ):
+        # presets:
+        # Zara70
+        # alignment to 500ms straddling start of movement
+        # data from F5
+        self.filename = filename
+        self.data     = None
+        
+    def load(self):
+        self.data = mat73.loadmat(self.filename)
+    
+    
+class View(Data):
+    def __init__(self,
+                 dobj:Data,
+                 dimension_grouping:Optional[List[List[int]]] = [[1,2],[0]],
+                 epoch_selection:List[int] = [4],
+                 array_selection:List[int] = [2,3],
+                 context_selection:List[str] = ['active']
+    ):
+        self.dobj = dobj
+        self.dimension_grouping = dimension_grouping
+        self.epoch_selection = epoch_selection
+        self.array_selection = array_selection
+        self.context_selection = context_selection
+        

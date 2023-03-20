4e087d16b8a6981a48d4df82da9bef3b38147980
jmgoodman <j.mike.goodman@gmail.com>
2023-03-20 14:11:12 +0100

    Update contrast_histogram_plotter.m

--- Analysis-Outputs/contrast_histogram_plotter.m
+++ Analysis-Outputs/contrast_histogram_plotter.m
@@ -33,67 +33,110 @@ for monkeyIdx = 1:numel(monkeyNames)
     end
 end
 
-% now plot the histograms
-for rotInd = 1:numel(areaNames)
-    aNames = circshift(areaNames,-rotInd);
-    for monkeyName = monkeyNames
-        monkeyName_ = monkeyName{1};
-        pooled = [];
-        nneur  = [];
-        figure
-        for areaIdx = 1:numel(aNames)
-            areaName_ = aNames{areaIdx};
-            alph = 0.2;
-            if areaIdx==numel(aNames)
-                alph = 1;
-            end
-            whichColor = strcmpi(colorStruct.labels,areaName_);
-            thisColor  = colorStruct.colors(whichColor,:);
-            hold all
-            h=histogram(data.(monkeyName_).(areaName_),-1:.1:1,'Normalization','probability',...
-                'facecolor',thisColor,'facealpha',alph,'edgecolor',[0 0 0],'edgealpha',alph);
-            pooled = vertcat(pooled,data.(monkeyName_).(areaName_)); %#ok<AGROW>
-            nneur = vertcat(nneur,numel(data.(monkeyName_).(areaName_)));
-        end
+for monkeyName = monkeyNames
+    monkeyName_ = monkeyName{1};
+    hc = [];
+    cmat = [];
+    for areaName = areaNames
+        areaName_ = areaName{1};
+        temp = histc(data.(monkeyName_).(areaName_),-1:.1:1);
+        hc = horzcat(hc,temp(:)); %#ok<AGROW>
         
-        nneur = circshift(nneur,rotInd); % de-rotate
-        nneur = flipud(nneur);
-        
-        legendNames = {'AIP','F5','M1'};
-        for legent = 1:numel(legendNames)
-            legendNames{legent} = [legendNames{legent},': N=',num2str(nneur(legent))];
-        end
-        
-        xlabel('Passive-Active Index')
-        ylabel('Fraction of units')
-        customlegend(legendNames,'colors',colorStruct.colors(2:end,:),...
-            'fontsize',16,'xoffset',-0.15)
-        
-        set(gca,'fontsize',16)
-        set(gcf,'paperposition',[0 0 6 6])
-        title(['Monkey ',upper(monkeyName_(1))])
-        box off, axis tight
-        print([monkeyName_,'_',aNames{3},'.svg'],'-dsvg')
-        
-        % also pool
-        if rotInd == 3
-            figure
-            whichColor = strcmpi(colorStruct.labels,'pooled');
-            thisColor  = colorStruct.colors(whichColor,:);
-            h=histogram(pooled,-1:.1:1,'Normalization','probability',...
-                'facecolor',thisColor,'facealpha',0.2,'edgecolor',[0 0 0],'edgealpha',1);
-            set(gca,'fontsize',16)
-            set(gcf,'paperposition',[0 0 6 6])
-            xlabel('Passive-Active Index')
-            ylabel('Fraction of units')
-            customlegend({['pooled: N=',num2str(sum(nneur))]},'colors',[0 0 0],...
-                'fontsize',16,'xoffset',-0.2)
-            title(['Monkey ',upper(monkeyName_(1))])
-            box off, axis tight
-            print([monkeyName_,'_','pooled','.svg'],'-dsvg')
-        end
+        whichColor = strcmpi(colorStruct.labels,areaName_);
+        thisColor  = colorStruct.colors(whichColor,:);
+        cmat = vertcat(cmat,thisColor); %#ok<AGROW>
     end
+    
+    figure,b=bar(-.95:.1:.95,hc(1:(end-1),:),1,'stacked');
+    for i = 1:3
+        set(b(i),'facecolor',cmat(i,:));
+    end
+    
+    box off, axis tight
+    xlim([-1 1])
+    
+    legendNames = {'AIP','F5','M1'};
+    for legent = 1:numel(legendNames)
+        legendNames{legent} = [legendNames{legent},': N=',num2str(numel(data.(monkeyName_).(legendNames{legent})))];
+    end
+    
+    xlabel('Passive-Active Index')
+    ylabel('Number of units')
+    customlegend(legendNames,'colors',colorStruct.colors(2:end,:),...
+        'fontsize',16,'xoffset',-0.15)
+    
+    set(gca,'fontsize',16)
+    set(gcf,'paperposition',[0 0 6 6])
+    title(['Monkey ',upper(monkeyName_(1))])
+    
+    print([monkeyName_,'_','stacked','.svg'],'-dsvg')
 end
+    
+        
+    
+
+%%
+% % now plot the histograms
+% for rotInd = 1:numel(areaNames)
+%     aNames = circshift(areaNames,-rotInd);
+%     for monkeyName = monkeyNames
+%         monkeyName_ = monkeyName{1};
+%         pooled = [];
+%         nneur  = [];
+%         figure
+%         for areaIdx = 1:numel(aNames)
+%             areaName_ = aNames{areaIdx};
+%             alph = 0.2;
+%             if areaIdx==numel(aNames)
+%                 alph = 1;
+%             end
+%             whichColor = strcmpi(colorStruct.labels,areaName_);
+%             thisColor  = colorStruct.colors(whichColor,:);
+%             hold all
+%             h=histogram(data.(monkeyName_).(areaName_),-1:.1:1,'Normalization','probability',...
+%                 'facecolor',thisColor,'facealpha',alph,'edgecolor',[0 0 0]);
+%             pooled = vertcat(pooled,data.(monkeyName_).(areaName_)); %#ok<AGROW>
+%             nneur = vertcat(nneur,numel(data.(monkeyName_).(areaName_)));
+%         end
+%         
+%         nneur = circshift(nneur,rotInd); % de-rotate
+%         nneur = flipud(nneur);
+%         
+%         legendNames = {'AIP','F5','M1'};
+%         for legent = 1:numel(legendNames)
+%             legendNames{legent} = [legendNames{legent},': N=',num2str(nneur(legent))];
+%         end
+%         
+%         xlabel('Passive-Active Index')
+%         ylabel('Fraction of units')
+%         customlegend(legendNames,'colors',colorStruct.colors(2:end,:),...
+%             'fontsize',16,'xoffset',-0.15)
+%         
+%         set(gca,'fontsize',16)
+%         set(gcf,'paperposition',[0 0 6 6])
+%         title(['Monkey ',upper(monkeyName_(1))])
+%         box off, axis tight
+%         print([monkeyName_,'_',aNames{3},'.svg'],'-dsvg')
+%         
+%         % also pool
+%         if rotInd == 3
+%             figure
+%             whichColor = strcmpi(colorStruct.labels,'pooled');
+%             thisColor  = colorStruct.colors(whichColor,:);
+%             h=histogram(pooled,-1:.1:1,'Normalization','probability',...
+%                 'facecolor',thisColor,'facealpha',0.2,'edgecolor',[0 0 0]);
+%             set(gca,'fontsize',16)
+%             set(gcf,'paperposition',[0 0 6 6])
+%             xlabel('Passive-Active Index')
+%             ylabel('Fraction of units')
+%             customlegend({['pooled: N=',num2str(sum(nneur))]},'colors',[0 0 0],...
+%                 'fontsize',16,'xoffset',-0.2)
+%             title(['Monkey ',upper(monkeyName_(1))])
+%             box off, axis tight
+%             print([monkeyName_,'_','pooled','.svg'],'-dsvg')
+%         end
+%     end
+% end
         
         
         

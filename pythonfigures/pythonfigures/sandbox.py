480d222a48ed13046fb2b1ed24063cb3c385e8a0
jmgoodman <j.mike.goodman@gmail.com>
2023-03-24 17:23:27 +0100

    looking over principal angles results and getting ready to do the same for alignment indices

--- pythonfigures/pythonfigures/sandbox.py
+++ pythonfigures/pythonfigures/sandbox.py
@@ -3,6 +3,17 @@ import pandas as pd
 import numpy as np
 import plotly.express as px # warning: "plotly" has a secret dependency that it doesn't install on its own: "packaging"!
 from sklearn.decomposition import PCA
+from scipy.linalg import subspace_angles, orth
+import json
+from json import JSONEncoder
+
+# define a class for parsing numpy arrays
+# https://pynative.com/python-serialize-numpy-ndarray-into-json/
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj,np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self,obj)
 
 
 sessions =  ['Moe46'] # ['Moe46','Moe50','Zara64','Zara68','Zara70']
@@ -93,13 +104,21 @@ for session in sessions:
         print(df_agg)
         
         # delta (within-class variance)
-        print(deltaquery)
+        # print(deltaquery)
         
-        qdel = Query(query=deltaquery,
-                  queryfile=False)
-        df_del = qdel.read()
+        # qdel = Query(query=deltaquery,
+        #           queryfile=False)
+        # df_del = qdel.read()
         
-        print(df_del)
+        # print(df_del)
+        
+        # full data pull (subquery)
+        print(subquery)
+        qsub = Query(query=subquery,
+                     queryfile=False)
+        df_sub = qsub.read()
+        
+        print(df_sub)
         
         # and now we have the dataframes needed to do LDA
         aggdata = dict()
@@ -110,21 +129,32 @@ for session in sessions:
         print(aggdata['active'].shape)
         print(aggdata['passive'].shape)
         
-        diffdata = dict()
-        diffdata['active'] = df_del[df_del['contextdiff']=='active'].iloc[:,:-2].to_numpy()
-        diffdata['passive'] = df_del[df_del['contextdiff']=='passive'].iloc[:,:-2].to_numpy()
+        # diffdata = dict()
+        # diffdata['active'] = df_del[df_del['contextdiff']=='active'].iloc[:,:-2].to_numpy()
+        # diffdata['passive'] = df_del[df_del['contextdiff']=='passive'].iloc[:,:-2].to_numpy()
         
-        print(diffdata)
-        print(diffdata['active'].shape)
-        print(diffdata['passive'].shape)
+        # print(diffdata)
+        # print(diffdata['active'].shape)
+        # print(diffdata['passive'].shape)
+        
+        subdata = dict()
+        subdata['active'] = df_sub[ df_sub['context']=='active' ].iloc[:,:-4].to_numpy()
+        subdata['passive'] = df_sub[ df_sub['context']=='active' ].iloc[:,:-4].to_numpy()
+        print(subdata)
+        print(subdata['active'].shape)
+        print(subdata['passive'].shape)
+        
+        # misnomer, it's just PCA applied to the trial averages (i.e., between-group variances)
+        LDAvecs = dict()
+        LDAvals = dict()
         
         for context in ['active','passive']:
-            neigs = aggdata[context].shape[0] - 1 # nobjs - 1 eigenvalues
+            neigs = aggdata[context].shape[0]-1 # nobjs - 1 eigenvalues
             B = np.cov( aggdata[context],rowvar=False )
-            W = np.cov( diffdata[context], rowvar=False )
-            W = W + 1e-6 * np.eye(W.shape[0])
+            # W = np.cov( diffdata[context], rowvar=False )
+            # W = W + 1e-6 * np.eye(W.shape[0])
             
-            D = np.linalg.solve(W,B)
+            # D = np.linalg.solve(W,B)
             
             # test symmetry (all values are real so no conjugates to worry about)
             # (test is failed, a skew-symmetric divided by a skew-symmetric does not give a skew-symmetric per se)
@@ -135,19 +165,106 @@ for session in sessions:
             print(context)
             print(B)
             print(B.shape)
-            print(W)
-            print(W.shape)
-            print(D)
-            print(D.shape)
+            # print(W)
+            # print(W.shape)
+            # print(D)
+            # print(D.shape)
             
             # can't use eigh due to the non-symmetry
-            vals,vecs = np.linalg.eig(D)
+            # only keep up to neigs, as all other dimensions beyond that will have zero magnitude (and be riddled with floating point problems)
+            vals,vecs = np.linalg.eig(B) # if we just use B, we get much better alignment than if we use D... this probably points to instabilities in the manifolds that can be extracted from LDA arising from the matrix division step
+            # ergo, let's just use B and ignore the "diff" query
+            # note, we still will want to use the same neigs parameter, as the rank of B is what limited the rank of D!
             vals = vals[:neigs]
             vecs = vecs[:,:neigs]
             
+            # extract the real components, as the computation of vals & vecs is corrupted by complex garbage resulting from floating point errors
+            vals = np.real(vals)
+            vecs = np.real(vecs)
             
             print('vals')
             print(vals)
             
             print('vecs')
             print(vecs)
+            
+            # save the vectors and values
+            LDAvecs[context] = vecs
+            LDAvals[context] = vals
+            
+            
+        # get principal angles (note: my custom method only work for orthonormal bases, and returns supra-1 eigenvalues for non-orthonormal bases, ergo it tends to underestimate the subspace angles as it overestimates the cosine projections)
+        # (that said, the svd method works when using scipy.linalg.orth to extract orthonormal basis sets)
+        # we should probably iterate this over # of vectors kept in each subspace tho
+        # (note: subspace angles apparently also work when there's a difference in the number of vectors in each subspace? wild. seems to work by finding the "best-case" subspace within the larger which matches up to the smaller)
+        out = dict()
+        out['subspace_dimension'] = list()
+        out['theta'] = list()
+        out['vals'] = dict()
+        out['vecs'] = dict()
+        out['vals']['active'] = list()
+        out['vals']['passive'] = list()
+        out['vecs']['active'] = list()
+        out['vecs']['passive'] = list()
+        
+        print(LDAvecs['active'].shape[1])
+        print(LDAvecs['passive'].shape[1])
+        maxvecs   = min(LDAvecs['active'].shape[1],LDAvecs['passive'].shape[1])
+        neurcount = LDAvecs['active'].shape[0]
+        
+        for ndims in range(1,maxvecs):
+            out['theta'] += [np.degrees( subspace_angles(LDAvecs['active'][:,:ndims],LDAvecs['passive'][:,:ndims])[::-1] )] # list the closest angles first! and use degrees, they're easier to read
+            out['subspace_dimension'] += [ndims]
+            
+        # contrast with random subspaces
+        niter = 1000
+        out['theta_noise'] = list()
+        for ndims in range(1,maxvecs):
+            temp = []
+            for iter in range(niter):
+                r = np.random.randn(neurcount,2*ndims) # no need to call orth here, subspace_angles does that internally
+                temp += [np.degrees( subspace_angles( r[:,:ndims],r[:,ndims:] )[::-1] )]
+            
+            out['theta_noise'] += [np.column_stack(temp).T]
+            
+        out['vals']['active'] = LDAvals['active']
+        out['vals']['passive'] = LDAvals['passive']
+        out['vecs']['active'] = LDAvecs['active']
+        out['vecs']['passive'] = LDAvecs['passive']
+            
+        # print(out)
+        
+        with open('testdata.json','w',encoding='utf-8') as f:
+            json.dump(out,f,ensure_ascii=False,indent=4,cls=NumpyArrayEncoder)
+            
+        # or try rtheta (weighted subspace angles)
+        badmethod = """
+        rtheta = list()
+        
+        maxvecs = min(LDAvecs['active'].shape[1],LDAvecs['passive'].shape[1])
+        
+        for ndims in range(1,maxvecs):
+            act = LDAvecs['active'][:,:ndims] #@ np.diag( np.sqrt( LDAvals['active'][:ndims] ) ) # scale dimensions by standard deviation, rather than variance, along each dimension
+            pas = LDAvecs['passive'][:,:ndims] #@ np.diag( np.sqrt( LDAvals['passive'][:ndims] ) )
+            S = np.linalg.svd(act.T @ pas)[1]
+            S[S>1] = 1
+            rtheta += [np.arccos(S[::-1])] # to match the ordering convention of scipy
+            
+        with open('testdata_selfangles.json','w',encoding='utf-8') as f:
+            json.dump(rtheta,f,ensure_ascii=False,indent=4,cls=NumpyArrayEncoder)"""
+        
+        # and compute alignment indices, too
+        # this requires a "raw" data query
+        
+        # remember: Jannik's paper tells us that poor classification performance is not enough! the manifolds have to have poor alignment, too!
+        # that said, we should compare both pre- and post-orthogonalization alignment values
+        # so:
+        # W's are actually quite well-aligned
+        # B's are less well-aligned
+        # and D, the LDA matrices, are not well-aligned at ALL
+        
+        # note: compare alignments in all the following cases:
+        # prior to orthogonalization
+        # after orthogonalization
+        # within the orthogonalized space (!)
+        
